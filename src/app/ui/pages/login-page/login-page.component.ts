266f300b6028900faf53c4d8ed341373c795a466
Daly <dalyna64@gmail.com>
2024-01-29 00:10:11 +0100

    save

--- src/app/ui/pages/login-page/login-page.component.ts
+++ src/app/ui/pages/login-page/login-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'login-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPage{
-
+  hide = true;
 }
--- src/app/ui/pages/register-page/register-page.component.ts
+++ src/app/ui/pages/register-page/register-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'register-page',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  hide = true;
+}
